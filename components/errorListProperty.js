0526d2d62bd89ee6288629d154385365845d1aff
jpsanabria37 <jpsanabria37@misena.edu.co>
2023-03-27 13:11:03 -0600

    dfghsdjfgdshjf

--- components/errorListProperty.js
+++ components/errorListProperty.js
@@ -1,13 +1,17 @@
 const ErrorListProperty = ({ errors }) => {
-    return (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
-            <ul>
-                {errors.map((error, index) => (
-                    <li key={index}>{error}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  if (!errors || errors.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+      <ul>
+        {errors.map((error, index) => (
+          <li key={index}>{error}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ErrorListProperty;
--- components/errorsList.js
+++ components/errorsList.js
@@ -1,19 +1,24 @@
-const ErrorsList = ( {errors}) => {
-    if (Object.keys(errors).length === 0) {
-        return null;
-    }
+const ErrorsList = ({ errors }) => {
+  if (Object.keys(errors).length === 0) {
+    return null;
+  }
+  if (!errors || errors.length === 0) {
+    return null;
+  }
 
-    return (
-        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong className="font-bold">Errores:</strong>
-            <ul className="list-disc ml-4 mt-2">
-                {Object.entries(errors).map(([key, value]) => (
-                    <li key={key}>{value.join(", ")}</li>
-                ))}
-            </ul>
-        </div>
-    );
-
-}
+  return (
+    <div
+      className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      role="alert"
+    >
+      <strong className="font-bold">Errores:</strong>
+      <ul className="list-disc ml-4 mt-2">
+        {Object.entries(errors).map(([key, value]) => (
+          <li key={key}>{value.join(", ")}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ErrorsList;
